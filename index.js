b0dbb5370a1afe32aaaeda6b6d5317eb5d959aa4
lifeoncode <nduduzo@yellogarden.co.za>
2023-02-07 16:56:50 +0200

    bug fix: converting formdata to JSON

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs-extra");
 const multer = require("multer");
 const excelToJson = require("convert-excel-to-json");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// middleware - use json and serve static files
+// middleware - cors to allow access from all domains, use json and serve static files
+// app.use(bodyParser.json());
+// for parsing application/xwww-
+// app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // file upload destination
-const upload = multer({ dest: "uploads/" });
-
+const uploads = multer({ dest: "uploads/" });
 // routes
-app.post("/read", upload.single("file"), (req, res) => {
+app.post("/upload", uploads.single("the-file"), (req, res) => {
   // handle a file uploaded - error 400 if user did not select a file to upload
   // otherwise set filepath as target for excel-to-json library to use as source
   // skip the first row in the file (it contains headings)
@@ -38,6 +44,7 @@ app.post("/read", upload.single("file"), (req, res) => {
     }
     // catch errors and respond with appropriate status code
   } catch (error) {
+    console.log("ERROR:\n", error);
     res.status(500).json(error);
   }
 });
---
+++ public/app.js
@@ -0,0 +1,28 @@
+const sendData = async (file) => {
+  const response = await fetch("/read", {
+    method: "post",
+    headers: { "Content-Type": "Application/json" },
+    body: JSON.stringify({ file }),
+  });
+  const responseData = await response.json();
+  console.log(responseData);
+  return responseData;
+};
+
+// form submit
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userFile = document.querySelector("#file-input").files[0];
+  const formData = new FormData();
+  formData.append("the-file", userFile);
+
+  fetch("http://localhost:5000/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.log("something went wrong:", error);
+    });
+});
